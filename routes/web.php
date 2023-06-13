44703f496b39d26b0f016c9c40310fba1863209c
rizkarhm <rizkarahma67@gmail.com~>
2023-06-14 01:25:58 +0700

    replace UserController with AkunController

--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,6 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\KendaraanController;
 use App\Http\Controllers\KontakController;
 use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\SendEmail;
-use App\Http\Controllers\SendWhatsapp;
-use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 // Route::get('/', function () {
@@ -40,10 +37,7 @@ Route::middleware('auth')->group(function () {
     Route::resource('dashboard', DashboardController::class);
 
     Route::resource('profile', ProfileController::class);
-    // Route::resource('user', UserController::class);
     Route::resource('customer', CustomerController::class);
-
-    // Route::get('/users', [UserController::class, 'view'])->name('users');
     Route::resource('akun', AkunController::class);
 
     Route::resource('booking', BookingController::class);
